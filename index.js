015ea5a42e517300a45b6329c7f260ed3e8a35f0
Asif-vai <mr3366533@gmail.com>
2026-01-16 18:25:23 +0600

    connect with database

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require('express')
+const mongoose = require('mongoose');
 const dotenv = require('dotenv')
 dotenv.config()
 const cors = require('cors')
 const port = process.env.PORT || 3030
 const app = express()
+
+
+async function main() {
+  await mongoose.connect(process.env.DB_URI);
+}
+
+main().then(()=>console.log(`server is connected with mongoose successfully`)).catch(err => console.log(err));
+
+
+
 app.get('/',(req,res)=>{
     res.send('Meta Blog Server Is Running......')
 })
