3637b00d23a164e6a52c76231fab834f9ea37a48
samrat-xyz <md.samrat61790@gmail.com>
2026-01-20 12:37:44 -0500

    create routes

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
-const express = require('express')
-const mongoose = require('mongoose');
-const dotenv = require('dotenv')
-dotenv.config()
-const cors = require('cors')
-const port = process.env.PORT || 3030
-const app = express()
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+const cors = require("cors");
+const port = process.env.PORT || 3030;
+const app = express();
 
+// routes
+const blogRoutes = require('./src/routes/blog.routes')
+app.use('/blogs',blogRoutes)
 
 async function main() {
   await mongoose.connect(process.env.DB_URI);
+  app.get("/", (req, res) => {
+    res.send("Meta Blog Server Is Running......");
+  });
 }
 
-main().then(()=>console.log(`server is connected with mongoose successfully`)).catch(err => console.log(err));
+main()
+  .then(() => console.log(`server is connected with mongoose successfully`))
+  .catch((err) => console.log(err));
 
-
-
-app.get('/',(req,res)=>{
-    res.send('Meta Blog Server Is Running......')
-})
-
-app.listen(port,()=>{
-    console.log(`server is running on port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+});
