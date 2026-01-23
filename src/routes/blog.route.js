f217d767abf4d945841c05ea7da8b53a75b7868e
samrat-xyz <md.samrat61790@gmail.com>
2026-01-23 16:16:02 +0600

    implement blog delete & update

--- src/routes/blog.route.js
+++ src/routes/blog.route.js
@@ -3,6 +3,8 @@ const BlogModel = require("../models/blog.model");
 
 const router = express.Router();
 
+// get all blogs
+
 router.get("/", async (req, res) => {
   try {
     const blogs = await BlogModel.find().sort({ createdAt: -1 });
@@ -19,6 +21,67 @@ router.get("/", async (req, res) => {
   }
 });
 
+//get single blog by id
+
+router.get("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const blog = await BlogModel.findById(id);
+    res.status(200).json({
+      success: true,
+      message: "fetch single blog by id",
+      blog,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "failed to fetch single blog",
+    });
+  }
+});
+
+// delete single blog
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const blog = await BlogModel.findByIdAndDelete(id);
+    res.status(200).json({
+      success: true,
+      message: "successfully data delete",
+      blog,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: "failed to delete blog",
+    });
+  }
+});
+
+// update single blog
+
+router.put("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const UpdatedBlogData = req.body;
+    const updatedBlog = await BlogModel.findByIdAndUpdate(id, UpdatedBlogData, {
+      new: true,
+    });
+    res.status(200).json({
+      success: true,
+      message: "Blog update successfully",
+      updatedBlog,
+    });
+  } catch (error) {
+    res.status(401).json({
+      success: false,
+      message: "failed to update blog",
+    });
+  }
+});
+
+// create or add blogs
 router.post("/add-blogs", async (req, res) => {
   try {
     const newBlog = new BlogModel(req.body);
